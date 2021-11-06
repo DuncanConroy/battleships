562552afd89f79c4f0598d3dffaa5f57dbea0a17
Daniel Bunte <daniel.bunte.84@gmail.com>
2021-11-06 08:20:53 +0100

    adds spring boot application files

---
+++ src/test/kotlin/com/danielbunte/battleships/BattleshipsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.danielbunte.battleships
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BattleshipsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}

54645bfe88d68c8ba3aa704b4e41a039a646645e
Daniel Bunte <daniel.bunte.84@gmail.com>
2021-11-06 08:06:37 +0100

    Adds vice versa tests for game board coordinates

--- src/main/kotlin/com/danielbunte/battleships/gameworld/GameBoard.kt
+++ src/main/kotlin/com/danielbunte/battleships/gameworld/GameBoard.kt
@@ -10,8 +10,13 @@ class GameBoard(width: Int, height: Int) {
     fun getCoordinates(coordinates: String): Pair<Int, Int> {
         val cleanedCoordinates = coordinates.replace(" ", "").uppercase(Locale.getDefault())
         val x = cleanedCoordinates[0].code - charCodeA
-        val y = parseInt(cleanedCoordinates[1].toString()) - 1
+        val y = parseInt(cleanedCoordinates.substring(1)) - 1
         return x to y
     }
 
+    fun getCoordinates(inputX: Int, inputY: Int): String {
+        val x = Char(inputX + charCodeA)
+        val y = inputY + 1
+        return "$x$y"
+    }
 }
--- src/test/kotlin/com/danielbunte/battleships/gameworld/GameBoardTests.kt
+++ src/test/kotlin/com/danielbunte/battleships/gameworld/GameBoardTests.kt
@@ -9,14 +9,15 @@ class GameBoardTests {
     @ParameterizedTest(name = "{0} translates to {1},{2}")
     @CsvSource(
         // human readable, x, y
-        "a1, 0, 0",
-        "A2, 0, 1",
-        "b1, 1, 0",
-        "B2, 1, 1",
-        "A 1, 0, 0",
-        "a   2, 0, 1",
-        "B  1, 1, 0",
+        "a1      , 0, 0",
+        "A2      , 0, 1",
+        "b1      , 1, 0",
+        "B2      , 1, 1",
+        "A 1     , 0, 0",
+        "a   2   , 0, 1",
+        "B  1    , 1, 0",
         "b      2, 1, 1",
+        "j   10  , 9, 9",
     )
     fun `human readable coordinates are translated into x,y coordinates`(input: String, expectedX: Int, expectedY: Int) {
         // given: a game board instance
@@ -25,11 +26,34 @@ class GameBoardTests {
         // B    #    #
         val classUnderTest = GameBoard(2, 2)
 
-        // when: A1 is requested
+        // when: input is requested
         val result = classUnderTest.getCoordinates(input)
 
         // then: expectedX, expectedY is returned
         assertEquals(result.first, expectedX)
         assertEquals(result.second, expectedY)
+    }
+
+    @ParameterizedTest(name = "{0}, {1} translates to {2}")
+    @CsvSource(
+        // human readable, x, y
+        "0, 0, A1",
+        "0, 1, A2",
+        "1, 0, B1",
+        "1, 1, B2",
+        "9, 6, J7",
+    )
+    fun `x,y coordinates are translated into human readable coordinates`(inputX: Int, inputY: Int, expected: String) {
+        // given: a game board instance
+        //      1    2
+        // A    #    #
+        // B    #    #
+        val classUnderTest = GameBoard(2, 2)
+
+        // when: inputX, inputY is requested
+        val result = classUnderTest.getCoordinates(inputX, inputY)
+
+        // then: expected is returned
+        assertEquals(result, expected)
     }
 }
